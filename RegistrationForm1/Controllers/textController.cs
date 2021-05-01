1cab7370743b6eed68d2f1528413fdcff55bfba1
sunitalakade24 <sunitalakade1999@gmail.com>
2021-05-01 19:16:43 +0530

    project initialization

---
+++ RegistrationForm1/Controllers/textController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationForm1.Models;
+
+namespace RegistrationForm1.Controllers
+{
+    public class textController : Controller
+    {
+        // GET: text
+        public ActionResult Index()
+        {
+            employe e1 = new employe { empid = 121, empname = "sunita", empemail = "sunitalakade55", emppassword = "121212", empmobile = "2147542873", empaddress = "pune" };
+
+            return View(e1);
+        }
+    }
+}
---
+++ RegistrationForm1/Models/employe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationForm1.Models
+{
+    public class employe
+    {
+        public int empid { get; set; }
+        public string empname { get; set; }
+        public string empemail { get; set; }
+        public string emppassword { get; set; }
+        public string empmobile { get; set; }
+        public string empaddress { get; set; }
+
+    }
+}
